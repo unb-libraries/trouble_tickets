91349b5d01f9afe0b679658f4b8d1ddccd33b3b4
JeremyMcDermott <jeremy.mcdermott@gmail.com>
2015-10-28 09:49:17 -0300

    Removed jQuery handled by Form API

--- js/trouble_tickets.js
+++ js/trouble_tickets.js
@@ -1,27 +1,5 @@
 /*global jQuery, document, window*/
 
 jQuery(document).ready(function () {
-    // Emulate old AJAX Trouble Tickets behaviours using jQuery 1.6+.
-    jQuery('.form-item-ticket-topic select').change(function () {
-        // New Feature Request selected hide 'Category' container and default to 'Feature Request' radio.
-        if (this.value.indexOf('New Systems') > -1) {
-            jQuery('input[value="Feature Request"]').prop('checked', true);
-            jQuery('.form-item-category').hide();
-        } else {
-            jQuery('.form-item-category').show();
-        }
-
-        // Repairs / Requests selected alter radio input labels.
-        if (this.value.indexOf('Library Maintenance') > -1) {
-            jQuery('input[value="Bug"] + label').text('Repair');
-            jQuery('input[value="Feature Request"] + label').text('Request');
-            jQuery('.form-item-building').show();
-        } else {
-            jQuery('input[value="Bug"] + label').text('Bug');
-            jQuery('input[value="Feature Request"] + label').text('Feature Request');
-            jQuery('.form-item-building').hide();
-            jQuery('select[name="building"] option:first').prop('selected', true);
-        }
-    });
     jQuery('html, body').animate({scrollTop : 0}, 'fast');
 });
