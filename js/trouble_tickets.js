4d1a31ab4a825467bf5e16748580ded1a5c53400
JeremyMcDermott <jeremy.mcdermott@gmail.com>
2015-10-27 22:52:01 -0300

    switched to Form API for redirects and category field visibility, update widgets for Category/Repair fields, added animated scroll to top on load

--- js/trouble_tickets.js
+++ js/trouble_tickets.js
@@ -3,32 +3,25 @@
 jQuery(document).ready(function () {
     // Emulate old AJAX Trouble Tickets behaviours using jQuery 1.6+.
     jQuery('.form-item-ticket-topic select').change(function () {
-        // Redirect select items tagged as 'eResource' to external lib.unb.ca form.
-        if (this.value.indexOf('eResources') > -1) {
-            window.location = 'http://www.lib.unb.ca/help/troubleticket.php';
+        // New Feature Request selected hide 'Category' container and default to 'Feature Request' radio.
+        if (this.value.indexOf('New Systems') > -1) {
+            jQuery('input[value="Feature Request"]').prop('checked', true);
+            jQuery('.form-item-category').hide();
         } else {
-            // Update legend text on select option when not redirecting.
-            var legend = 'Trouble tickets for ';
-            legend += jQuery('.form-item-ticket-topic select option:selected').text();
-            jQuery('span.fieldset-legend').text(legend);
+            jQuery('.form-item-category').show();
         }
-
-       // New Feature Request selected hide 'Category' container and default to 'Feature Request' radio.
-       if (this.value.indexOf('New Systems') > -1) {
-         jQuery('input[value="Feature Request"]').prop('checked', true);
-       }
 
         // Repairs / Requests selected alter radio input labels.
         if (this.value.indexOf('Library Maintenance') > -1) {
             jQuery('input[value="Bug"] + label').text('Repair');
             jQuery('input[value="Feature Request"] + label').text('Request');
+            jQuery('.form-item-building').show();
         } else {
             jQuery('input[value="Bug"] + label').text('Bug');
             jQuery('input[value="Feature Request"] + label').text('Feature Request');
+            jQuery('.form-item-building').hide();
+            jQuery('select[name="building"] option:first').prop('selected', true);
         }
-
-        // Handle legend arrow toggle icons, make form visible on select.
-        jQuery('fieldset.form-wrapper').addClass('wrapper-expanded').removeClass('wrapper-collapsed');
-        jQuery('.fieldset-wrapper').slideDown('fast');
     });
+    jQuery('html, body').animate({scrollTop : 0}, 'fast');
 });
