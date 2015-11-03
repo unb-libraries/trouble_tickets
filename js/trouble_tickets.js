c548ba66e769edddc91bfd7a0e7a63ceb699fb14
JeremyMcDermott <jeremy.mcdermott@gmail.com>
2015-11-03 17:27:56 -0400

    rebind legend slidetoggle behaviour after completed form fieldset ajax replacements

--- js/trouble_tickets.js
+++ js/trouble_tickets.js
@@ -3,3 +3,11 @@
 jQuery(document).ready(function () {
     jQuery('html, body').animate({scrollTop : 0}, 'fast');
 });
+
+jQuery(document).ajaxSuccess(function() {
+    // toggle expanding/collapsing fieldset content via legend click
+    jQuery("fieldset legend").click(function() {
+        jQuery(this).parent().toggleClass("wrapper-expanded wrapper-collapsed");
+        jQuery(this).next(".fieldset-wrapper").slideToggle("fast");
+    });
+});
